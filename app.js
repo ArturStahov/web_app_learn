4b1b4c768ee55e992c77ca596e3d697a61f8cfeb
ArturStahov <vtemy.at.ua@gmail.com>
2020-10-12 20:36:26 +0300

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+
+})
+
+
+app.listen(7777, () => {
+    console.log("server is ranning")
+});
