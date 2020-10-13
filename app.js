48a9628616053b6f52c4767b9ce98239548a7a22
ArturStahov <johndoe@example.com>
2020-10-13 12:36:55 +0300

    complete

--- app.js
+++ app.js
@@ -2,15 +2,43 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars');
 
-app.set('view engine', 'handlebars');
+const products = require('./products.json');
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+const PORT = process.env.PORT || 7777;
+
+app.use(express.static('public'));
+
+//регестрируем движок хандлебарс
+app.set('view engine', 'hbs');
+//передаем настройки и указуем разширение для viev engine
+app.engine('hbs',handlebars({
+    extname:'hbs'
+}))
+
+
+app.get('/', (req, res)=> {
+    res.render('home',{pageTitle:'home'});
+
+})
+app.get('/about', (req, res)=> {
+    res.render('about',{pageTitle:'about'})
+
+})
+app.get('/products', (req, res)=> {
+    res.render('products',{products:products,cssFileName:'products'})
 
 })
 
+app.get('/product/:productId', (req, res) => {
+    console.log(req.params);
+  
+    const product = products.find(p => p.id === req.params.productId);
+  
+    res.render('product', { product });
+  });
 
-app.listen(7777, () => {
+
+app.listen(PORT, () => {
     console.log("server is ranning")
 });
 //
